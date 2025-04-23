e7f70d485ca8266effc2ba0b0f681eb72be55cfc
zwh <wenhanz166@gmail.com>
2025-04-23 12:42:06 +0800

    my commit

--- Main.java
+++ Main.java
@@ -2,9 +2,8 @@ import view.login.HomeFrame;
 
 public class Main {
     public static void main(String[] args) {
-
         HomeFrame homeFrame = new HomeFrame();
         homeFrame.setVisible(true);
-
+        int a = 0;
     }
 }
